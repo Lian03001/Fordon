017793cfa2730f5f91e40880ce9e32eb7626e72e
Linus <73946659+Lian03001@users.noreply.github.com>
2020-12-16 15:02:09 +0100

    Add files via upload

---
+++ Fordon 2/Fordon 2/Fordon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordon_2
+{
+    class Fordon
+    {
+        private string Tillverkare;
+        private string Modell;
+        private int Årsmodell;
+
+        public void setTillverkare(string t){Tillverkare = t;}
+        public string getTillverkare(){return Tillverkare;}
+
+        public void setModell(string m){Modell = m;}
+        public string getModell(){return Modell;}
+
+        public void setÅrsmodell(int å){Årsmodell = å;}
+        public int getÅrsmodell(){return Årsmodell;}
+    }
+}
---
+++ Fordon 2/Fordon 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordon_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fordon SAAB = new Fordon();
+            SAAB.setTillverkare("SAAB");
+            SAAB.setModell("95a");
+            SAAB.setÅrsmodell(99);
+
+            Console.WriteLine(SAAB.getTillverkare());
+            Console.WriteLine(SAAB.getModell());
+            Console.WriteLine(SAAB.getÅrsmodell());
+        }
+    }
+}
